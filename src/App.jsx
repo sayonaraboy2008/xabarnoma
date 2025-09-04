f4088c783cf1a0844d024ff581f83abfebf2f5d7
sayonaraboy2008 <barkamolbaxtiyarovich@gmail.com>
2025-09-04 18:06:15 +0500

    Chat componentga emoji qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import "./App.css";
 import List from "../src/components/list/List";
 import Chat from "../src/components/chat/Chat";
 import Detail from "../src/components/detail/Detail";
+import Login from "./components/login/Login";
 function App() {
+  const user = false;
+
   return (
     <>
       <div className="container">
-        <List />
-        <Chat />
-        <Detail />
+        {user ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (
+          <Login />
+        )}
       </div>
     </>
   );
---
+++ src/components/login/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./login.css";
+function Login() {
+  return <div className="login">login</div>;
+}
+
+export default Login;
